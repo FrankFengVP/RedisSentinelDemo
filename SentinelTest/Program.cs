1a47ffd806e4c959a68ce489c17e7bf658d469e4
Frank Feng <frankfuse@hotmail.com>
2020-01-21 17:35:02 +0800

    Init

--- SentinelTest/Program.cs
+++ SentinelTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StackExchange.Redis;
 
 namespace SentinelTest
 {
@@ -10,6 +11,21 @@ namespace SentinelTest
     {
         static void Main(string[] args)
         {
+            //只需要在这里提供所有Redis实例的连接串即可, 哨兵实例可忽略
+            var connection = ConnectionMultiplexer.Connect("127.0.0.1:6380,127.0.0.1:6381,127.0.0.1:6382");
+            var db = connection.GetDatabase();
+
+            Console.WriteLine("---------------------------Redis Sentinel Test---------------------------");
+            Console.WriteLine(connection.GetStatus());
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            db.StringSet("sentiel", 1);
+
+            while (true)
+            {
+                var x = Console.ReadLine();
+                db.StringSet("sentiel", x);
+            }
         }
     }
 }
